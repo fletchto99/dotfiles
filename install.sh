a6d82a09794f39d3b7658cc046911f853a9f302f
Matthew Langlois <fletchto99@gmail.com>
2017-07-10 00:43:10 -0700

    More comfertable key repeat settings

--- install.sh
+++ install.sh
@@ -178,8 +178,8 @@ then
 	defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 	# Set a blazingly fast keyboard repeat rate
-	defaults write NSGlobalDomain KeyRepeat -int 1
-	defaults write NSGlobalDomain InitialKeyRepeat -int 10
+	defaults write NSGlobalDomain KeyRepeat -int 2
+	defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
 	# Disable the “Are you sure you want to open this application?” dialog
 	defaults write com.apple.LaunchServices LSQuarantine -bool false
