c5227e7de7d4c6be1e22f5a4b2dc77cc8b8e5706
Matt Langlois <fletchto99@github.com>
2018-11-13 16:40:58 -0700

    Add some more software

--- bootstrap.sh
+++ bootstrap.sh
@@ -40,9 +40,10 @@ if hash brew 2>/dev/null; then
 		# Tap brew repos
 		brew tap caskroom/cask
 		brew tap buo/cask-upgrade
+		brew tap homebrew/cask-drivers
 
 		# Install some default apps
-		brew cask install 1password adobe-acrobat-reader bartender bettertouchtool binary-ninja burp-suite datagrip db-browser-for-sqlite deluge disablemonitor discord docker encryptme etcher filezilla google-chrome hyperdock idafree intellij-idea iterm2 java8 kaleidoscope keka keybase mactex megasync messenger metasploit microsoft-office moom paragon-extfs plex-media-player runescape skim slack softu2f speedcrunch spotify steam sublime-text teamviewer vlc vmware-fusion whatsapp wireshark zoomus
+		brew cask install 1password adobe-acrobat-reader bartender bettertouchtool binary-ninja burp-suite datagrip db-browser-for-sqlite deluge disablemonitor discord docker encryptme etcher filezilla google-chrome hyperdock idafree intellij-idea iterm2 java8 kaleidoscope kap keka keybase logitech-gaming-software mactex messenger metasploit microsoft-office moom nextcloud paragon-extfs plex-media-player runescape skim slack softu2f speedcrunch spotify steam sublime-text teamviewer vlc vmware-fusion whatsapp wireshark zoomus
 		((COUNTER++))
 	fi
 
